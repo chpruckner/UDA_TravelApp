1fb978fc60ee1abf5e0c9b2e41908629f9e9d23c
chpruckner <christiane.pruckner@gmail.com>
2021-09-22 12:10:16 +0200

    adapt test

--- __test__/handleDate.test.js
+++ __test__/handleDate.test.js
@@ -2,7 +2,7 @@ import { handleDate } from "../src/client/js/handleDate";
 
 describe("Testing date handler", () => {
   test("Testing if 2031-12-01 is in the future", () => {
-    expect(handleDate("2031-12-01")).toEqual({ check: true, days: 3723 });
+    expect(handleDate("2031-12-01")).toEqual({ check: true, days: 3724 });
   });
 
   test("Testing if 2021-09-01 lies in the past", () => {
